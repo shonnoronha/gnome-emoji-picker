c2766fe89a9242732a6fc0a82dcdace295cf93b5
shonnoronha <shon.noronha@gmail.com>
2025-02-23 17:15:15 +0530

    add key binder

--- include/emoji_picker.h
+++ include/emoji_picker.h
@@ -2,19 +2,23 @@
 #define EMOJI_PICKER_H
 
 #include <gtkmm.h>
+#include <keybinder-3.0/keybinder.h>
 
 class EmojiPicker : public Gtk::Window {
   public:
     EmojiPicker();
-
-  protected:
-    void load_emojis();
-    void on_emoji_selected(Gtk::FlowBoxChild *child);
+    virtual ~EmojiPicker();
 
   private:
     Gtk::Box m_main_box;
     Gtk::ScrolledWindow m_scrolled_window;
     Gtk::FlowBox m_flow_box;
+
+    void load_emojis();
+    void on_emoji_selected(Gtk::FlowBoxChild *child);
+    static void hotkey_handler(const char *keystring, void *user_data);
+    void toggle_window();
+    void insert_to_active_window(const std::string &text);
 };
 
-#endif 
+#endif
--- src/emoji_picker.cpp
+++ src/emoji_picker.cpp
@@ -1,8 +1,12 @@
 #include "emoji_picker.h"
 #include "emoji_loader.h"
 #include "gtkmm/button.h"
+#include "gtkmm/clipboard.h"
 #include "gtkmm/enums.h"
-#include <iostream>
+#include <X11/Xlib.h>
+#include <X11/extensions/XTest.h>
+#include <gdk/gdkx.h>
+#include <keybinder-3.0/keybinder.h>
 #include <nlohmann/json.hpp>
 
 EmojiPicker::EmojiPicker() {
@@ -11,8 +15,8 @@ EmojiPicker::EmojiPicker() {
     set_position(Gtk::WIN_POS_CENTER);
     set_type_hint(Gdk::WINDOW_TYPE_HINT_DIALOG);
     set_keep_above();
-
     m_main_box.set_orientation(Gtk::ORIENTATION_VERTICAL);
+
     m_main_box.set_spacing(6);
     m_main_box.set_border_width(10);
     add(m_main_box);
@@ -36,6 +40,46 @@ EmojiPicker::EmojiPicker() {
     m_scrolled_window.add(m_flow_box);
     m_main_box.pack_start(m_scrolled_window, true, true);
     show_all();
+
+    keybinder_init();
+    keybinder_bind("<Super>period", &EmojiPicker::hotkey_handler, this);
+}
+
+EmojiPicker::~EmojiPicker() {
+    keybinder_unbind("<Super>period", &EmojiPicker::hotkey_handler);
+}
+
+void EmojiPicker::toggle_window() {
+    if (is_visible()) {
+        hide();
+    } else {
+        show();
+        present();
+    }
+}
+
+void EmojiPicker::hotkey_handler(const char *keystring, void *user_data) {
+    auto *picker = static_cast<EmojiPicker *>(user_data);
+    picker->toggle_window();
+}
+
+void EmojiPicker::insert_to_active_window(const std::string &text) {
+    Display *display = XOpenDisplay(nullptr);
+    if (!display)
+        return;
+
+    auto clipboard = Gtk::Clipboard::get();
+    clipboard->set_text(text);
+
+    XTestFakeKeyEvent(display, XKeysymToKeycode(display, XK_Control_L), True,
+                      0);
+    XTestFakeKeyEvent(display, XKeysymToKeycode(display, XK_v), True, 0);
+    XTestFakeKeyEvent(display, XKeysymToKeycode(display, XK_v), False, 0);
+    XTestFakeKeyEvent(display, XKeysymToKeycode(display, XK_Control_L), False,
+                      0);
+
+    XCloseDisplay(display);
+    hide();
 }
 
 void EmojiPicker::load_emojis() {
@@ -67,13 +111,10 @@ void EmojiPicker::on_emoji_selected(Gtk::FlowBoxChild *child) {
     auto button = dynamic_cast<Gtk::Button *>(child->get_child());
     if (!button)
         return;
+
     std::string emoji = button->get_label();
-    std::cout << "Button selected: " << emoji << std::endl;
 
-    auto clipboard = Gtk::Clipboard::get();
-    clipboard->set_text(emoji);
+    insert_to_active_window(emoji);
 
-    std::string cmd = "xdotool type " + emoji;
-    system(cmd.c_str());
     button->grab_focus();
 }
